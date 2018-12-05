6e58c308969b6148c1320409d240cbaa703572e3
masterUNG <phrombutr@gmail.com>
2018-12-05 13:02:04 +0700

    Design UI Authen finish

--- app/src/main/java/masterung/th/in/androidthai/mysupermay/MainActivity.java
+++ app/src/main/java/masterung/th/in/androidthai/mysupermay/MainActivity.java
@@ -9,5 +9,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragment To Activity
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment())
+                    .commit();
+        }
+
+
+
+    }   // Main Method
+
+}   // Main Class
