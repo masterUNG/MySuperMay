eda155a3e2b7845fbd6700269221657b079514b7
masterUNG <phrombutr@gmail.com>
2018-12-05 15:56:59 +0700

    Create Toolbar Success

---
+++ app/src/main/java/masterung/th/in/androidthai/mysupermay/HomeFragment.java
@@ -0,0 +1,42 @@
+package masterung.th.in.androidthai.mysupermay;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+        Toolbar toolbar = getView().findViewById(R.id.toolbarHome);
+
+
+
+    }   // Main Method
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+}   // Main Class
