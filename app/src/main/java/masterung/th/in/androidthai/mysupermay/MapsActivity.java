b90910e1ac39f10cdf21bd9a3ba11dcfbe2d0874
masterUNG <phrombutr@gmail.com>
2018-12-05 14:40:49 +0700

    onMapClick OK

--- app/src/main/java/masterung/th/in/androidthai/mysupermay/MapsActivity.java
+++ app/src/main/java/masterung/th/in/androidthai/mysupermay/MapsActivity.java
@@ -1,5 +1,12 @@
 package masterung.th.in.androidthai.mysupermay;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -13,6 +20,9 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LocationManager locationManager;
+    private Criteria criteria;
+    private double latADouble = 0, lngADouble = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,25 +32,150 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+//        Setup Logcation
+        setupLogcation();
+
+
+    }   // Main Method
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        discoveryLocation();
+
+    }
+
+    private void discoveryLocation() {
+
+//        For NetWork
+        Location netWorkLocation = myFindLocation(LocationManager.NETWORK_PROVIDER);
+        if (netWorkLocation != null) {
+            latADouble = netWorkLocation.getLatitude();
+            lngADouble = netWorkLocation.getLongitude();
+        }
+
+//        For GPS
+        Location gpsLocation = myFindLocation(LocationManager.GPS_PROVIDER);
+        if (gpsLocation != null) {
+            latADouble = gpsLocation.getLatitude();
+            lngADouble = gpsLocation.getLongitude();
+        }
+
+
+
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        locationManager.removeUpdates(locationListener);
+
+    }
+
+    public Location myFindLocation(String providerString) {
+
+        Location location = null;
+
+        if (locationManager.isProviderEnabled(providerString)) {
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return null;
+            }
+            locationManager.requestLocationUpdates(providerString, 1000, 10, locationListener);
+            location = locationManager.getLastKnownLocation(providerString);
+
+        }
+
+
+        return location;
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    public LocationListener locationListener  = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+
+            latADouble = location.getLatitude();
+            lngADouble = location.getLongitude();
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+
+    private void setupLogcation() {
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        Setup CenterMap
+        setupCenterMap();
+
+//        Map Controller
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+
+                mMap.clear();
+                createMarker(latLng);
+
+
+
+            }
+        });
+
+
+    }   // onMapReady
+
+    private void setupCenterMap() {
+        if (latADouble != 0) {
+
+            LatLng latLng = new LatLng(latADouble, lngADouble);
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16));
+            createMarker(latLng);
+
+        } else {
+            discoveryLocation();
+        }
     }
-}
+
+    private void createMarker(LatLng latLng) {
+
+        MarkerOptions markerOptions = new MarkerOptions().position(latLng);
+        mMap.addMarker(markerOptions);
+
+    }
+
+}   // Main Class
